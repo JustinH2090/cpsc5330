5a9525c7d558fe3ce6881fae9f900304608eddc1
Justin Hensley <jeh0162@auburn.edu>
2025-06-05 00:35:58 -0400

    finished assignment 3

---
+++ assignment 3/assignment 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment 3
+//
+//  Created by Justin.Hensley on 2025-06-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ assignment 3/assignment 3Tests/assignment_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  assignment_3Tests.swift
+//  assignment 3Tests
+//
+//  Created by Justin.Hensley on 2025-06-03.
+//
+
+import Testing
+@testable import assignment_3
+
+struct assignment_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
