4e3ea5469173fb3341d2cc4670a2b0d6251bc4dc
Justin Hensley <jeh0162@auburn.edu>
2025-06-10 04:08:12 -0400

    Initial layout

---
+++ assignment 4/assignment 4/assignment 4Tests/assignment_4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  assignment_4Tests.swift
+//  assignment 4Tests
+//
+//  Created by Justin.Hensley on 2025-06-10.
+//
+
+import Testing
+@testable import assignment_4
+
+struct assignment_4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
