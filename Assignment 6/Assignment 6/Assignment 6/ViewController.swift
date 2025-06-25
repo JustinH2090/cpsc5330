87111dc7664ba14add75bf9c42073aae0dcbaf53
Justin Hensley <jeh0162@auburn.edu>
2025-06-25 16:35:43 -0400

    ui changes

---
+++ Assignment 6/Assignment 6/Assignment 6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 6
+//
+//  Created by Justin.Hensley on 2025-06-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Assignment 6/Assignment 6/Assignment 6Tests/Assignment_6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_6Tests.swift
+//  Assignment 6Tests
+//
+//  Created by Justin.Hensley on 2025-06-25.
+//
+
+import Testing
+@testable import Assignment_6
+
+struct Assignment_6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
