a69c42b5411aafaf50cd48e6f970a1c18f277e12
Justin Hensley <jeh0162@auburn.edu>
2025-07-03 20:33:50 -0400

    added initial ui layout

---
+++ Assignment Bonus/Assignment Bonus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment Bonus
+//
+//  Created by Justin.Hensley on 2025-07-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Assignment Bonus/Assignment BonusTests/Assignment_BonusTests.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_BonusTests.swift
+//  Assignment BonusTests
+//
+//  Created by Justin.Hensley on 2025-07-03.
+//
+
+import Testing
+@testable import Assignment_Bonus
+
+struct Assignment_BonusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
